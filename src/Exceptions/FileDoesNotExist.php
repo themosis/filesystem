676f1bc6ace1ddc51d37a925ad81f1542ee3a165
Julien <julien@themosis.com>
2024-05-24 07:22:27 +0200

    Update licenses using default comment format.

--- src/Exceptions/FileDoesNotExist.php
+++ src/Exceptions/FileDoesNotExist.php
@@ -1,10 +1,8 @@
 <?php
 
-/**
- * SPDX-FileCopyrightText: 2024 Julien Lambé <julien@themosis.com>
- *
- * SPDX-License-Identifier: GPL-3.0-or-later
- */
+// SPDX-FileCopyrightText: 2024 Julien Lambé <julien@themosis.com>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 declare(strict_types=1);
 
