57f1170509fc89aa48e7aa89018dde10b8a5111b
Julien Lambé <julien@themosis.com>
2025-06-06 21:00:23 +0200

    Update documentation using org-mode syntax. Update license.

--- src/Exceptions/CannotReadFromFile.php
+++ src/Exceptions/CannotReadFromFile.php
@@ -1,6 +1,7 @@
 <?php
 
 // SPDX-FileCopyrightText: 2024 Julien Lambé <julien@themosis.com>
+// SPDX-FileCopyrightText: 2025 Julien Lambé <julien@themosis.com>
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
